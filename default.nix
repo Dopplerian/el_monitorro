87ff649a33f07544df1f8c0b6eda964472130b92
Dopplerian <dopplerian+git@proton.me>
2024-11-29 15:32:10 +0100

    add meta in `default.nix`

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 {
   pkgs ? import <nixpkgs> { },
+  lib,
   ...
 }:
 let
@@ -11,11 +12,20 @@ pkgs.rustPlatform.buildRustPackage {
   cargoLock.lockFile = ./Cargo.lock;
   doCheck = false;
   src = pkgs.lib.cleanSource ./.;
+
   nativeBuildInputs = with pkgs; [
     pkg-config
   ];
+
   buildInputs = with pkgs; [
     openssl
     postgresql.lib
   ];
+
+  meta = {
+    description = "El Monitorro is an RSS, Atom and JSON feed reader as a Telegram bot.";
+    homepage = "https://github.com/ayrat555/el_monitorro";
+    license = lib.licenses.mit;
+    mainProgram = "el_monitorro";
+  };
 }
