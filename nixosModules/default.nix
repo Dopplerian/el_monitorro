4e3554e5c6a9378691f98848727157e007f8d99f
Dopplerian <dopplerian+git@proton.me>
2024-12-24 18:42:56 +0100

    move to screaming snake case

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -6,93 +6,93 @@ in
 {
   options.services.elmonitorro = {
     enable = lib.mkEnableOption "El Monitorro's service";
-    databaseUrl = lib.mkOption {
+    DATABASE_URL = lib.mkOption {
       example = "postgres://username:password@localhost/el_monitorro";
       description = "DB Postgres url. If let unset, a local DB will be setup.";
       type = lib.types.str;
     };
-    telegramToken = lib.mkOption {
+    TELEGRAM_TOKEN = lib.mkOption {
       example = "6666618370:AAGx5YhNQvUG4eUcQXN-OB_a09ZzYl6aaaa";
       description = "Telegram token to be used. This is required.";
       type = lib.types.str;
     };
-    databasePoolSize = lib.mkOption {
+    DATABASE_POOL_SIZE = lib.mkOption {
       default = 5;
       example = 10;
       description = "The maximum number of connections for global connection pool (global per binary except if {option}`services.elMonitorro.allBinaries` is set to true).";
       type = lib.types.int;
     };
-    allBinaries = lib.mkOption {
+    ALL_BINARIES = lib.mkOption {
       default = false;
       example = true;
       description = "If true, all services will be started in the main binary.";
       type = lib.types.bool;
     };
-    telegramBotHandle = lib.mkOption {
+    TELEGRAM_BOT_HANDLE = lib.mkOption {
       example = "el_monitorro_bot";
       description = "This value is used during parsing of commands. If you set autocompletion menu for your bot, the bot will understand commands like `/subscribe@<handle>` along with just `/subscribe`.";
       type = lib.types.str;
     };
-    subscriptionLimit = lib.mkOption {
+    SUBSCRIPTION_LIMIT = lib.mkOption {
       default = 20;
       example = 40;
       description = ""; # TODO
       type = lib.types.int;
     };
-    filterLimit = lib.mkOption {
+    FILTER_LIMIT = lib.mkOption {
       default = 20;
       example = 40;
       description = "The maximum number of filter words that can be set per subscription or as the global filter.";
       type = lib.types.str;
     };
-    syncIntervalSeconds = lib.mkOption {
+    SYNC_INTERVAL_SECONDS = lib.mkOption {
       default = 60;
       example = 40;
       description = "The bot tries to sync feeds every `SYNC_INTERVAL_SECONDS` seconds.";
       type = lib.types.int;
     };
-    syncWorkersNumber = lib.mkOption {
+    SYNC_WORKERS_NUMBER = lib.mkOption {
       default = 1;
       example = 4;
       description = "The number of workers to sync feeds.";
       type = lib.types.int;
     };
-    deliverIntervalSeconds = lib.mkOption {
+    DELIVER_INTERVAL_SECONDS = lib.mkOption {
       default = 60;
       example = 120;
       description = "The bot tries to deliver new feed items every `DELIVER_INTERVAL_SECONDS` seconds.";
       type = lib.types.int;
     };
-    deliverWorkersNumber = lib.mkOption {
+    DELIVER_WORKERS_NUMBER = lib.mkOption {
       default = 1;
       example = 4;
       description = "The number of workers to deliver updates.";
       type = lib.types.int;
     };
-    cleanIntervalSeconds = lib.mkOption {
+    CLEAN_INTERVAL_SECONDS = lib.mkOption {
       default = 3600;
       example = 1800;
       description = "The bot cleans old feed items and feeds without subscriptions every `CLEAN_INTERVAL_SECONDS` seconds.";
       type = lib.types.int;
     };
-    cleanWorkersNumbers = lib.mkOption {
+    CLEAN_WORKERS_NUMBERS = lib.mkOption {
       default = 1;
       example = 4;
       description = "The number of workers to remove old data.";
       type = lib.types.int;
     };
-    ownerTelegramId = lib.mkOption {
+    OWNER_TELEGRAM_ID = lib.mkOption {
       example = "your_id_here"; # TODO: does this need the @?
       description = "If this value is set, the bot will process commands only from the specified user id.";
       type = lib.types.str;
     };
-    requestTimeout = lib.mkOption {
+    REQUEST_TIMEOUT = lib.mkOption {
       default = 5;
       example = 10;
       description = "Timeout in seconds for feed syncing requests.";
       type = lib.types.int;
     };
-    adminTelegramId = lib.mkOption {
+    ADMIN_TELEGRAM_ID = lib.mkOption {
       example = "your_admin_id_here"; # TODO: same issue as above
       description = "If this value is set, /info command with stats is available for the specified account.";
       type = lib.types.str;
